2e1e7b41f83ff81970b3a59f10645bbfbd65c957
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-05-13 12:27:12 -0400

    repair item

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -14,7 +14,11 @@ function fail(item) {
 }
 
 function repair(item) {
+  if (item.durability >= 0 && item.durability <100 && item.enhancement >= 0 && item.enhancement <= 20) {
+    return {...item, durability: 100};
+  } else {
   return { ...item };
+  }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,42 @@
-const enhancer = require('./enhancer.js');
+const {repair, succeed, fail, get} = require('./enhancer.js');
+
+describe('repair item function', () => {
+        it('should accept an obj and return 100 durability', () => {
+            expect(repair({
+                name: "itemA",
+                durability: 5,
+                enhancement: 15,
+                })).toEqual({
+                    name: "itemA",
+                    durability: 100,
+                    enhancement: 15,
+                })
+            })
+        });
+
+describe('enhancement success', () => {
+    it('should accept an obj with enhancement increase by 1', () => {
+        expect(succeed({
+            name: "itemB",
+            durability: 0,
+            enhancement: 5,
+        })).toEqual({
+            name: "itemB",
+            durability: 5,
+            enhancement: 6,
+        })
+    });
+    it('the item enhancement level is 20, the enhancement is not changed', () => {
+        expect(succeed({
+            name: "itemB",
+            durability: 0,
+            enhancement: 20,
+        })).toEqual({
+            name: "itemB",
+            durability: 0,
+            enhancement: 20,
+        })
+    })
+})
+
 // test away!
